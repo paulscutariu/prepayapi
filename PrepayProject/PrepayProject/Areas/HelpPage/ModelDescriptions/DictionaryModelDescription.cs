2714030d995ab78f855bf2caf00c4c16d2943027
paullfc <paul.scutariu@gmail.com>
2017-11-18 16:25:09 +0200

    added project

---
+++ PrepayProject/PrepayProject/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PrepayProject.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ PrepayProject/PrepayProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrepayProject.WebApiApplication" Language="C#" %>
